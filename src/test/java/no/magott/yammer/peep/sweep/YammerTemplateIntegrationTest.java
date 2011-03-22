1577541b6ba09334732d44677728fdf9bd2a51d3
Morten Andersen-Gott <morten.andersengott@gmail.com>
2011-03-22 14:03:14 +0100

    refined tests and added test for empty page

--- src/test/java/no/magott/yammer/peep/sweep/YammerTemplateIntegrationTest.java
+++ src/test/java/no/magott/yammer/peep/sweep/YammerTemplateIntegrationTest.java
@@ -25,43 +25,48 @@ public class YammerTemplateIntegrationTest {
 
 	@Autowired
 	private YammerOperations yammerTemplate;
-	
+
 	private static String replyId;
 
 	@Test
-	public void canListMessages(){
+	public void canListMessages() {
 		List<YammerMessage> yams = yammerTemplate.listMessages();
 		System.out.println(yams);
 	}
-	
+
 	@Test
-	public void canListUsers(){
-		List<User> users = yammerTemplate.listAllUsers(2);
-		assertThat(users.size(), equalTo(50));
-		for (User user : users) {
-			System.out.println(user);
-		}
+	public void canListUsers() {
+		final int FIRST_PAGE = 1; // Page number starts at 1
+		List<User> users = yammerTemplate.listAllUsers(FIRST_PAGE);
+		assertThat(users.size(), equalTo(50)); // Yammer provides pages of 50
 	}
-	
+
+	@Test
+	public void emptyPageReturnsEmptyList() {
+		final int INVALID_PAGE_NUMBER = 100000; //Big page number should be empty
+		List<User> allUsers = yammerTemplate.listAllUsers(INVALID_PAGE_NUMBER);
+		assertThat(allUsers.size(), equalTo(0));
+	}
+
 	@Test
 	@Ignore("This will post to yammer, use with care")
 	public void canPostMessage() {
-		YammerMessage message = yammerTemplate.postNewPlainMessage("@morten-andersen-gott is sorry about the spam, but it isn't like Yammer provides a dev environment");
+		YammerMessage message = yammerTemplate
+				.postNewPlainMessage("@morten-andersen-gott is sorry about the spam, but it isn't like Yammer provides a dev environment");
 		assertThat(message.getRepliedToId(), nullValue());
 		assertThat(message.getId(), notNullValue());
 		replyId = message.getId();
 		System.out.println(replyId);
 	}
-	
+
 	@Test
 	@Ignore("This will post to yammer, use with care")
-	public void canPostReply(){
-		System.out.println("ReplyId: "+replyId);
+	public void canPostReply() {
+		System.out.println("ReplyId: " + replyId);
 		YammerMessage reply = yammerTemplate.postPlainReply("this is morten again, doing a reply", replyId);
 		assertThat(reply.getRepliedToId(), notNullValue());
 		assertThat(reply.getId(), notNullValue());
 		assertThat(reply.getId(), not(equalTo(replyId)));
 	}
-	
 
 }
