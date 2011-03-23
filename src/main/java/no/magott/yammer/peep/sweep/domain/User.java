81354d2971af1e7a21beff94464779cceb4b3ddd
Morten Andersen-Gott <morten.andersengott@gmail.com>
2011-03-23 20:36:39 +0100

    added e-mail property

--- src/main/java/no/magott/yammer/peep/sweep/domain/User.java
+++ src/main/java/no/magott/yammer/peep/sweep/domain/User.java
@@ -1,5 +1,8 @@
 package no.magott.yammer.peep.sweep.domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.codehaus.jackson.annotate.JsonProperty;
@@ -11,27 +14,86 @@ public class User {
 
 	@JsonProperty
 	private String id;
-	
+
 	@JsonProperty
 	private String name;
-	
+
 	@JsonProperty
 	private String url;
 	
+	@JsonProperty
+	private Contact contact;
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public String getUrl() {
 		return url;
 	}
-	
-	public String getSuspendUrl(){
+
+	public String getSuspendUrl() {
 		return String.format(SUSPEND_URL, id);
 	}
 	
-	public String toString(){
-		return new ToStringBuilder(this).append("Username", name).append("suspendurl", getSuspendUrl()).toString();
+	public Contact getContact() {
+		return contact;
 	}
-	
+
+	public String toString() {
+		ToStringBuilder builder =  new ToStringBuilder(this).append("Username", name)
+		.append("suspendurl", getSuspendUrl());
+		for (Email email : getContact().getEmailAdresses()) {
+			builder.append("Email",email.toString());
+		}
+		return builder.toString();
+	}
+
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	public static class Contact {
+		@JsonProperty("email_addresses")
+		private Email[] emailAddresses;
+
+		public List<Email> getEmailAdresses() {
+			return Arrays.asList(emailAddresses);
+		}
+	}
+
+	public static class Email {
+		@JsonProperty
+		private String type;
+		@JsonProperty
+		private String address;
+
+		public String getAddress() {
+			return address;
+		}
+
+		public String getType() {
+			return type;
+		}
+
+		@Override
+		public String toString() {
+			return type + " : " + address;
+		}
+	}
+
+	public static class Builder {
+		private String name;
+		private String id;
+
+		public Builder id(String id) {
+			this.id = id;
+			return this;
+		}
+
+		public User build() {
+			User user = new User();
+			user.id = id;
+			user.name = name;
+			return user;
+		}
+	}
+
 }
